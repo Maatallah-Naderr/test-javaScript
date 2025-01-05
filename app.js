cc2d21aead9dbe2d4f3808e326e94b7132d5f6b1
Maatallah-Naderr <nadermaatallah8@gmail.com>
2025-01-05 19:26:06 +0100

    premier pas javaScript

---
+++ app.js
@@ -0,0 +1,28 @@
+// calcul de bmi john
+let massJohn = 92
+let tallJohn = 1.95
+let bmiJohn = massJohn/ (tallJohn**2)
+console.log(bmiJohn)
+
+// calcul de bmi de mark
+let massMark=78
+let tallMark=1.69
+let bmiMark= massMark/(tallJohn**2)
+console.log(bmiMark)
+
+//comparison en utilison variable boolen
+let markHeigherBmi=bmiMark < bmiJohn
+console.log(markHeigherBmi)
+//2eme façon
+ massJohn = 95
+ tallJohn = 1.88
+ bimJohn = massJohn/ (tallJohn*tallJohn)
+console.log(bimJohn)
+
+ massMark= 85
+ tallMark= 1.76
+ bmiMark= massMark/(tallMark**2)
+console.log(bmiMark)
+markHeigherBmi= bmiMark < bimJohn
+console.log(markHeigherBmi)
+
